e568640a03ae54a5fc6005bbf0c8568bdfb568ab
danieldotcomcoder <daniel_shdeed@hotmail.com>
2024-09-07 12:07:25 +0300

    Add timeout

--- src/tests/HeroSection.test.jsx
+++ src/tests/HeroSection.test.jsx
@@ -1,7 +1,11 @@
 import { Selector } from 'testcafe';
 
 fixture `Check if the HeroSection component exists`
-    .page`http://localhost:5173`; 
+    .page`http://localhost:5173`
+    .requestHooks()
+    .timeouts({
+        pageRequestTimeout: 10000
+    });
 
 test('Check if the component exists', async t => {
     const component = Selector('.hero-section'); 
