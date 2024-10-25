bd51730986ac8ad5f16315f85252e8092655b1ba
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-10-25 17:42:22 +0100

    feat: font style added

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import hero_image from "../../assets/heroImage.png";
+import coin1 from "../../assets/coin1.png";
+import coin2 from "../../assets/coin2.png";
+import coin3 from "../../assets/coin3.png";
 import "./HeroSection.css";
 
+const statsData = [
+  { icon: coin1, number: 15, label: "Supported Chains" },
+  { icon: coin2, number: 250, label: "Supported DEX" },
+  { icon: coin3, number: 150, label: "Supported Tokens" },
+];
+
 const HeroSection = () => {
+  const [email, setEmail] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(email);
+  }
+
   return (
     <section data-testid='herosection1' className='hero-section'>
       <div className='content-container'>
@@ -15,11 +32,30 @@ const HeroSection = () => {
           Groundbreaking Business Model That Merges Gaming With Fintech
         </p>
 
-        <form action=''>
-          <input type='email' placeholder='Enter your email to get started' />
+        <form onSubmit={handleSubmit}>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='Enter your email to get started'
+          />
           <button type='submit'>+</button>
         </form>
+
+        <div className='statistics'>
+          {statsData.map((list, idx) => (
+            <figure key={idx}>
+              <img src={list.icon} alt={list.label} />
+              <figcaption>
+                <h3>{list.number}</h3>
+                <p>{list.label}</p>
+              </figcaption>
+            </figure>
+          ))}
+        </div>
       </div>
+
       <div className='image-container'>
         <img src={hero_image} alt='hero image' />
       </div>
