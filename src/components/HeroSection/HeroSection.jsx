db4465c6514ac10f62ac9c62a184da3590854025
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-10-25 15:56:14 +0100

    feat: hero section created

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,28 @@
+import hero_image from "../../assets/heroImage.png";
+import "./HeroSection.css";
+
 const HeroSection = () => {
   return (
     <section data-testid='herosection1' className='hero-section'>
-      HeroSection
+      <div className='content-container'>
+        <h1>
+          Integration Of <br />
+          Blockchain Technology <br />
+          <span>With The Gaming World</span>
+        </h1>
+        <p>
+          Apex Games Is A Pioneering Force In The Gaming Industry, Introducing A
+          Groundbreaking Business Model That Merges Gaming With Fintech
+        </p>
+
+        <form action=''>
+          <input type='email' placeholder='Enter your email to get started' />
+          <button type='submit'>+</button>
+        </form>
+      </div>
+      <div className='image-container'>
+        <img src={hero_image} alt='hero image' />
+      </div>
     </section>
   );
 };
