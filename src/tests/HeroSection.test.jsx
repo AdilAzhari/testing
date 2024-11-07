a6f3d4a8d0789e68ea0cd9f695e574b7abb207e3
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-11-07 16:03:05 +0100

    update: test case updated

--- src/tests/HeroSection.test.jsx
+++ src/tests/HeroSection.test.jsx
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
-import HeroSection from '../components/HeroSection/HeroSection';
+import { render } from "@testing-library/react";
+import HeroSection from "../components/HeroSection/HeroSection";
 
-test('Hero Section Exists', () => {
+test("Hero Section Exists", () => {
   const { getByTestId } = render(<HeroSection />);
-  expect(getByTestId('herosection1').textContent).toBe('HeroSection');
+  expect(getByTestId("herosection1").textContent).toBe(
+    "Integration Of Blockchain Technology With The Gaming WorldApex Games Is A Pioneering ..."
+  );
 });
-
