5086536a6b679ae88a4f6546ecd18181bbeb9b9a
PhilipOyelegbin <philipoyelegbin@gmail.com>
2024-11-07 16:35:10 +0100

    update: test case updated

--- src/tests/HeroSection.test.jsx
+++ src/tests/HeroSection.test.jsx
@@ -1,9 +1,15 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import HeroSection from "../components/HeroSection/HeroSection";
 
-test("Hero Section Exists", () => {
-  const { getByTestId } = render(<HeroSection />);
-  expect(getByTestId("herosection1").textContent).toBe(
-    "Integration Of Blockchain Technology With The Gaming WorldApex Games Is A Pioneering ..."
+test("Hero Section renders correct heading text", () => {
+  render(<HeroSection />);
+
+  const headingElement = screen.getByText(
+    /Integration Of\s+Blockchain Technology/i
+  );
+
+  expect(headingElement).toBeInTheDocument();
+  expect(headingElement).toHaveTextContent(
+    /Integration Of\s+Blockchain Technology/i
   );
 });
