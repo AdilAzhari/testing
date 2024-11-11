0cf10f2dd0f2430de4c4380435e5371dd2f59e2a
Najibullah Jafari <najib2020202020@gmail.com>
2024-11-11 11:17:52 +0430

    Add style footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
+import "./App.css";
+import "../src/assets/Footer/style/footer.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import HomePage from './pages/HomePage';
-
+import HomePage from "./pages/HomePage";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
