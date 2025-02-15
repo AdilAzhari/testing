e23cf2bc3fa11ded3a9c82b32f136de60ff5578b
Ramzi Karkoub <ramzikarkoub@ramzis-imac.lan>
2025-02-14 21:39:56 -0500

    Complete Key Feature Section

--- src/components/KeyInnovationSection/KeyInnovationSection.jsx
+++ src/components/KeyInnovationSection/KeyInnovationSection.jsx
@@ -1,107 +1,94 @@
+import React from "react";
 import "./KeyInnovationSection.css";
 import BitcoinTechnology from "../../assets/BitcoinTechnology.png";
 import BitcoinTechnology1 from "../../assets/BitcoinTechnology1.png";
 import BitcoinTechnology2 from "../../assets/BitcoinTechnology2.png";
+import LinkButton from "./LinkButton.jsx";
 
-import React from "react";
-import LinkButton from "./LinkButton";
-
-const KeyInnovatioinCard = ({
-  borderLinkStyle,
-  borderArrowStyle,
-  link,
-  colour,
-  size,
-  verticalDisplay,
+const KeyInnovationCard = ({
   image,
-  fullWidth,
   title,
   description,
+  size,
+  colour,
+  borderStyle,
+  horizontalDisplay,
 }) => {
   return (
     <div
-      className={`keyInnovation-card ${
-        size === "half" ? "half-width" : "full-width"
-      }`}
+      className={`keyInnovation-card ${size}`}
+      style={{ backgroundImage: `url(${image})` }}
     >
-      <img src={image} alt="" className="keyInnovation-card-image" />
-
       <div
         className={`keyInnovation-card-overlay ${
-          verticalDisplay === "vertical" ? "verticalDisplay" : ""
+          horizontalDisplay ? "horizontalDisplay" : ""
         }`}
       >
-        <div
-          className={`keyInnovation-title-desc ${
-            fullWidth === "full-width" ? "full-width" : ""
-          } `}
-        >
+        <div className="keyInnovation-card-title-description">
           <h3 className="keyInnovation-card-title">{title}</h3>
-          <p className="keyInnovation-card-description">{description}</p>
+          <p
+            className={`keyInnovation-card-description ${
+              horizontalDisplay ? "description" : ""
+            }`}
+          >
+            {description}
+          </p>
         </div>
-
-        <LinkButton
-          borderLinkStyle={borderLinkStyle}
-          borderArrowStyle={borderArrowStyle}
-          link={link}
-          label={"Learn More"}
-          dynamicColor={`121.76deg, ${colour} 15.41%, ${colour} 81.48%`}
-        />
+        <div className="btn">
+          <LinkButton
+            borderLinkStyle={borderStyle}
+            borderArrowStyle={borderStyle}
+            link="#"
+            label={"Learn More"}
+            dynamicColor={`121.76deg, ${colour} 15.41%, ${colour} 81.48%`}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
-function KeyInnovationSection() {
+const KeyInnovationSection = () => {
   return (
     <section className="keyInnovation-section">
       <hr />
       <div className="keyInnovation-header-container">
-        <div>
-          <h5>OUR KEY INNOVATIONS</h5>
-          <h3>
-            The Key Innovations By APEX In The <span>Gaming World</span>
-          </h3>
-        </div>
+        <h5>OUR KEY INNOVATIONS</h5>
+        <h3>
+          The Key Innovations By APEX In The <span>Gaming World</span>
+        </h3>
       </div>
       <div className="keyInnovation-card-container">
-        <KeyInnovatioinCard
-          borderLinkStyle={"1px solid #f60"}
-          borderArrowStyle={"1px solid #f60"}
-          link={"/"}
-          colour={"#FF1616"}
-          size="full"
+        <KeyInnovationCard
           image={BitcoinTechnology}
           title="Integration of Blockchain Technology"
           description="By incorporating blockchain, we provide a secure, transparent, and decentralized platform for managing digital currencies, enhancing trust and user engagement."
+          size="full"
+          colour="#FF1616"
+          borderStyle="1px solid #FF1616"
+          horizontalDisplay={true}
         />
-        <KeyInnovatioinCard
-          borderLinkStyle={"1px solid #f60"}
-          borderArrowStyle={"1px solid #f60"}
-          link={"/"}
-          colour={"#FF1616"}
-          size="half"
-          image={BitcoinTechnology1}
-          verticalDisplay="vertical"
-          fullWidth="full-width"
-          title="Dual Utility of APEX Tokens"
-          description="APEX tokens offer in-game benefits and contribute to real-world applications like affordable housing. This dual utility adds value to the tokens and supports social causes."
-        />
-        <KeyInnovatioinCard
-          borderLinkStyle={"1px solid #f60"}
-          borderArrowStyle={"1px solid #f60"}
-          link={"/"}
-          colour={"#FF1616"}
-          size="half"
-          verticalDisplay="vertical"
-          image={BitcoinTechnology2}
-          fullWidth="full-width"
-          title="Affordable Housing Initiative"
-          description="APEX tokens offer in-game benefits and contribute to real-world applications like affordable housing. This dual utility adds value to the tokens and supports social causes."
-        />
+        <div className="keyInnovation-row">
+          <KeyInnovationCard
+            image={BitcoinTechnology1}
+            title="Dual Utility of APEX Tokens"
+            description="APEX tokens offer in-game benefits and contribute to real-world applications like affordable housing. This dual utility adds value to the tokens and supports social causes."
+            size="half"
+            colour="#DCB105"
+            borderStyle="1px solid #DCB105"
+          />
+          <KeyInnovationCard
+            image={BitcoinTechnology2}
+            title="Affordable Housing Initiative"
+            description="APEX tokens offer in-game benefits and contribute to real-world applications like affordable housing. This dual utility adds value to the tokens and supports social causes."
+            size="half"
+            colour="#004AAD"
+            borderStyle="1px solid #004AAD"
+          />
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default KeyInnovationSection;
